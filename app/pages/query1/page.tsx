9b92a503758ebba95775909a5d8fae8a4667392f
Marshall Chan <marshallchan666@outlook.com>
2023-05-26 17:13:26 +0800

    feat: add animation for the dataset

--- app/pages/query1/page.tsx
+++ app/pages/query1/page.tsx
@@ -14,36 +14,48 @@ export default function Page() {
       <div className="mt-[90px]">
         <div className="mx-[70px] grid auto-cols-max grid-cols-12">
           <Dataset
+            mode="down"
+            delay={0.1}
             // eslint-disable-next-line react/no-children-prop
             children={'Traffic Source'}
             svg={<SvgIcon href="traffic_source"></SvgIcon>}
             col={'col-start-5'}></Dataset>
 
           <Dataset
+            mode="left"
+            delay={0.2}
             // eslint-disable-next-line react/no-children-prop
             children={'Session Quality'}
             svg={<SvgIcon href="session_quality"></SvgIcon>}
             col={'col-start-1'}></Dataset>
 
           <Dataset
+            mode="right"
+            delay={0.2}
             // eslint-disable-next-line react/no-children-prop
             children={'Geo Location'}
             svg={<SvgIcon href="geo_location"></SvgIcon>}
             col={'col-start-9'}></Dataset>
 
           <Dataset
+            mode="up"
+            delay={0.1}
             // eslint-disable-next-line react/no-children-prop
             children={'Time Spent'}
             svg={<SvgIcon href="time_spent"></SvgIcon>}
             col={'col-start-5 -translate-y-[55px]'}></Dataset>
 
           <Dataset
+            mode="right"
+            delay={0.2}
             // eslint-disable-next-line react/no-children-prop
             children={'eCommerce Action'}
             svg={<SvgIcon href="shopping-bag"></SvgIcon>}
             col={'col-start-2'}></Dataset>
 
           <Dataset
+            mode="left"
+            delay={0.2}
             // eslint-disable-next-line react/no-children-prop
             children={'User Tenure'}
             svg={<SvgIcon href="user"></SvgIcon>}
--- components/common/Dataset/index.tsx
+++ components/common/Dataset/index.tsx
@@ -1,24 +1,29 @@
 import { FC, ReactNode } from 'react';
+import MotionDiv from '../MotionDiv';
 
 type TProps = {
   children: ReactNode;
   svg: ReactNode;
   col: string;
+  mode: 'up' | 'down' | 'left' | 'right';
+  delay: number;
 };
 
-const Dataset: FC<TProps> = ({ children, svg, col }) => {
+const Dataset: FC<TProps> = ({ children, svg, col, mode, delay }) => {
   return (
     <div className={col}>
-      <div className="w-[20.5rem]">
-        <div className="flex justify-center">
-          <div className="relative h-[170px] w-[170px] rounded-full border-[6px] border-gblue-normal p-10 shadow-xl">
-            {svg}
+      <MotionDiv mode={mode} delay={delay}>
+        <div className="w-[20.5rem]">
+          <div className="flex justify-center">
+            <div className="relative h-[170px] w-[170px] rounded-full border-[6px] border-gblue-normal p-10 shadow-xl">
+              {svg}
+            </div>
           </div>
+          <p className="mt-7 text-center text-4xl text-gblue-normal">
+            {children}
+          </p>
         </div>
-        <p className="mt-7 text-center text-4xl text-gblue-normal">
-          {children}
-        </p>
-      </div>
+      </MotionDiv>
     </div>
   );
 };
