d3084aed5affc8420354f7b2be0010fb3180bc68
Marshall Chan <marshallchan666@outlook.com>
2023-06-06 00:42:03 +0800

    feat: add animation for square and circle

--- app/pages/results/page.tsx
+++ app/pages/results/page.tsx
@@ -16,25 +16,85 @@ export default function Page() {
           <ResultsHeader
             left_results={
               <>
-                <Square left={'left-[242px]'} top={'top-[320px]'}></Square>
-                <Square left={'left-[360px]'} top={'top-[352px]'}></Square>
-                <Square left={'left-[178px]'} top={'top-[410px]'}></Square>
-                <Square left={'left-[291px]'} top={'top-[429px]'}></Square>
-                <Square left={'left-[231px]'} top={'top-[536px]'}></Square>
-                <Square left={'left-[343px]'} top={'top-[514px]'}></Square>
+                <Square
+                  startLeft={242}
+                  startTop={320}
+                  endLeft={905}
+                  endTop={240}></Square>
+                <Square
+                  startLeft={360}
+                  startTop={352}
+                  endLeft={240}
+                  endTop={320}></Square>
+                <Square
+                  startLeft={178}
+                  startTop={410}
+                  endLeft={384}
+                  endTop={288}></Square>
+                <Square
+                  startLeft={291}
+                  startTop={429}
+                  endLeft={184}
+                  endTop={410}></Square>
+                <Square
+                  startLeft={231}
+                  startTop={536}
+                  endLeft={288}
+                  endTop={430}></Square>
+                <Square
+                  startLeft={343}
+                  startTop={514}
+                  endLeft={384}
+                  endTop={400}></Square>
               </>
             }
             right_results={
               <>
-                <Circle left={'left-[776px]'} top={'top-[252px]'}></Circle>
-                <Circle left={'left-[686px]'} top={'top-[319px]'}></Circle>
-                <Circle left={'left-[808px]'} top={'top-[341px]'}></Circle>
-                <Circle left={'left-[634px]'} top={'top-[418px]'}></Circle>
-                <Circle left={'left-[757px]'} top={'top-[429px]'}></Circle>
-                <Circle left={'left-[881px]'} top={'top-[417px]'}></Circle>
-                <Circle left={'left-[684px]'} top={'top-[518px]'}></Circle>
-                <Circle left={'left-[765px]'} top={'top-[545px]'}></Circle>
-                <Circle left={'left-[850px]'} top={'top-[524px]'}></Circle>
+                <Circle
+                  startLeft={776}
+                  startTop={252}
+                  endLeft={128}
+                  endTop={224}></Circle>
+                <Circle
+                  startLeft={686}
+                  startTop={319}
+                  endLeft={686}
+                  endTop={320}></Circle>
+                <Circle
+                  startLeft={808}
+                  startTop={341}
+                  endLeft={808}
+                  endTop={340}></Circle>
+                <Circle
+                  startLeft={634}
+                  startTop={418}
+                  endLeft={562}
+                  endTop={420}></Circle>
+                <Circle
+                  startLeft={757}
+                  startTop={429}
+                  endLeft={719}
+                  endTop={429}></Circle>
+                <Circle
+                  startLeft={881}
+                  startTop={417}
+                  endLeft={881}
+                  endTop={417}></Circle>
+                <Circle
+                  startLeft={684}
+                  startTop={518}
+                  endLeft={618}
+                  endTop={540}></Circle>
+                <Circle
+                  startLeft={765}
+                  startTop={545}
+                  endLeft={720}
+                  endTop={606}></Circle>
+                <Circle
+                  startLeft={850}
+                  startTop={524}
+                  endLeft={176}
+                  endTop={575}></Circle>
               </>
             }></ResultsHeader>
           {/* Final Positions */}
@@ -80,26 +140,90 @@ export default function Page() {
           <ResultsHeader
             left_results={
               <>
-                <Square left={'left-[340px]'} top={'top-[265px]'}></Square>
-                <Square left={'left-[242px]'} top={'top-[320px]'}></Square>
-                <Square left={'left-[360px]'} top={'top-[352px]'}></Square>
-                <Square left={'left-[178px]'} top={'top-[410px]'}></Square>
-                <Square left={'left-[291px]'} top={'top-[429px]'}></Square>
-                <Square left={'left-[231px]'} top={'top-[536px]'}></Square>
-                <Square left={'left-[343px]'} top={'top-[514px]'}></Square>
+                <Square
+                  startLeft={340}
+                  startTop={265}
+                  endLeft={905}
+                  endTop={260}></Square>
+                <Square
+                  startLeft={242}
+                  startTop={320}
+                  endLeft={60}
+                  endTop={280}></Square>
+                <Square
+                  startLeft={360}
+                  startTop={352}
+                  endLeft={373}
+                  endTop={274}></Square>
+                <Square
+                  startLeft={178}
+                  startTop={410}
+                  endLeft={373}
+                  endTop={373}></Square>
+                <Square
+                  startLeft={291}
+                  startTop={429}
+                  endLeft={44}
+                  endTop={417}></Square>
+                <Square
+                  startLeft={231}
+                  startTop={536}
+                  endLeft={363}
+                  endTop={597}></Square>
+                <Square
+                  startLeft={343}
+                  startTop={514}
+                  endLeft={830}
+                  endTop={496}></Square>
               </>
             }
             right_results={
               <>
-                <Circle left={'left-[776px]'} top={'top-[252px]'}></Circle>
-                <Circle left={'left-[686px]'} top={'top-[319px]'}></Circle>
-                <Circle left={'left-[808px]'} top={'top-[341px]'}></Circle>
-                <Circle left={'left-[634px]'} top={'top-[418px]'}></Circle>
-                <Circle left={'left-[757px]'} top={'top-[429px]'}></Circle>
-                <Circle left={'left-[881px]'} top={'top-[417px]'}></Circle>
-                <Circle left={'left-[684px]'} top={'top-[518px]'}></Circle>
-                <Circle left={'left-[765px]'} top={'top-[545px]'}></Circle>
-                <Circle left={'left-[850px]'} top={'top-[524px]'}></Circle>
+                <Circle
+                  startLeft={776}
+                  startTop={252}
+                  endLeft={36}
+                  endTop={156}></Circle>
+                <Circle
+                  startLeft={686}
+                  startTop={319}
+                  endLeft={686}
+                  endTop={180}></Circle>
+                <Circle
+                  startLeft={808}
+                  startTop={341}
+                  endLeft={808}
+                  endTop={340}></Circle>
+                <Circle
+                  startLeft={634}
+                  startTop={418}
+                  endLeft={562}
+                  endTop={420}></Circle>
+                <Circle
+                  startLeft={757}
+                  startTop={429}
+                  endLeft={719}
+                  endTop={429}></Circle>
+                <Circle
+                  startLeft={881}
+                  startTop={417}
+                  endLeft={881}
+                  endTop={417}></Circle>
+                <Circle
+                  startLeft={684}
+                  startTop={518}
+                  endLeft={618}
+                  endTop={540}></Circle>
+                <Circle
+                  startLeft={765}
+                  startTop={545}
+                  endLeft={763}
+                  endTop={597}></Circle>
+                <Circle
+                  startLeft={850}
+                  startTop={524}
+                  endLeft={330}
+                  endTop={196}></Circle>
               </>
             }></ResultsHeader>
           {/* Final Positions */}
@@ -146,25 +270,85 @@ export default function Page() {
           <ResultsHeader
             left_results={
               <>
-                <Square left={'left-[242px]'} top={'top-[320px]'}></Square>
-                <Square left={'left-[360px]'} top={'top-[352px]'}></Square>
-                <Square left={'left-[178px]'} top={'top-[410px]'}></Square>
-                <Square left={'left-[291px]'} top={'top-[429px]'}></Square>
-                <Square left={'left-[231px]'} top={'top-[536px]'}></Square>
-                <Square left={'left-[343px]'} top={'top-[514px]'}></Square>
+                <Square
+                  startLeft={242}
+                  startTop={320}
+                  endLeft={905}
+                  endTop={260}></Square>
+                <Square
+                  startLeft={360}
+                  startTop={352}
+                  endLeft={165}
+                  endTop={395}></Square>
+                <Square
+                  startLeft={178}
+                  startTop={410}
+                  endLeft={96}
+                  endTop={296}></Square>
+                <Square
+                  startLeft={291}
+                  startTop={429}
+                  endLeft={755}
+                  endTop={490}></Square>
+                <Square
+                  startLeft={231}
+                  startTop={536}
+                  endLeft={748}
+                  endTop={597}></Square>
+                <Square
+                  startLeft={343}
+                  startTop={514}
+                  endLeft={844}
+                  endTop={597}></Square>
               </>
             }
             right_results={
               <>
-                <Circle left={'left-[776px]'} top={'top-[252px]'}></Circle>
-                <Circle left={'left-[686px]'} top={'top-[319px]'}></Circle>
-                <Circle left={'left-[808px]'} top={'top-[341px]'}></Circle>
-                <Circle left={'left-[634px]'} top={'top-[418px]'}></Circle>
-                <Circle left={'left-[757px]'} top={'top-[429px]'}></Circle>
-                <Circle left={'left-[881px]'} top={'top-[417px]'}></Circle>
-                <Circle left={'left-[684px]'} top={'top-[518px]'}></Circle>
-                <Circle left={'left-[765px]'} top={'top-[545px]'}></Circle>
-                <Circle left={'left-[850px]'} top={'top-[524px]'}></Circle>
+                <Circle
+                  startLeft={776}
+                  startTop={252}
+                  endLeft={36}
+                  endTop={156}></Circle>
+                <Circle
+                  startLeft={686}
+                  startTop={319}
+                  endLeft={686}
+                  endTop={180}></Circle>
+                <Circle
+                  startLeft={808}
+                  startTop={341}
+                  endLeft={808}
+                  endTop={340}></Circle>
+                <Circle
+                  startLeft={634}
+                  startTop={418}
+                  endLeft={580}
+                  endTop={475}></Circle>
+                <Circle
+                  startLeft={757}
+                  startTop={429}
+                  endLeft={881}
+                  endTop={417}></Circle>
+                <Circle
+                  startLeft={881}
+                  startTop={417}
+                  endLeft={373}
+                  endTop={436}></Circle>
+                <Circle
+                  startLeft={684}
+                  startTop={518}
+                  endLeft={256}
+                  endTop={513}></Circle>
+                <Circle
+                  startLeft={765}
+                  startTop={545}
+                  endLeft={176}
+                  endTop={575}></Circle>
+                <Circle
+                  startLeft={850}
+                  startTop={524}
+                  endLeft={325}
+                  endTop={575}></Circle>
               </>
             }></ResultsHeader>
           {/* Final Positions */}
@@ -210,36 +394,112 @@ export default function Page() {
           <ResultsHeader
             left_results={
               <>
-                <Square left={'left-[340px]'} top={'top-[265px]'}></Square>
-                <Square left={'left-[280px]'} top={'top-[200px]'}></Square>
-                <Square left={'left-[270px]'} top={'top-[340px]'}></Square>
-                <Square left={'left-[200px]'} top={'top-[260px]'}></Square>
-                <Square left={'left-[150px]'} top={'top-[320px]'}></Square>
-                <Square left={'left-[360px]'} top={'top-[352px]'}></Square>
-                <Square left={'left-[178px]'} top={'top-[410px]'}></Square>
-                <Square left={'left-[291px]'} top={'top-[429px]'}></Square>
-                <Square left={'left-[231px]'} top={'top-[536px]'}></Square>
-                <Square left={'left-[343px]'} top={'top-[514px]'}></Square>
+                <Square
+                  startLeft={340}
+                  startTop={265}
+                  endLeft={882}
+                  endTop={264}></Square>
+                <Square
+                  startLeft={280}
+                  startTop={200}
+                  endLeft={839}
+                  endTop={363}></Square>
+                <Square
+                  startLeft={270}
+                  startTop={340}
+                  endLeft={930}
+                  endTop={341}></Square>
+                <Square
+                  startLeft={200}
+                  startTop={260}
+                  endLeft={810}
+                  endTop={471}></Square>
+                <Square
+                  startLeft={150}
+                  startTop={320}
+                  endLeft={897}
+                  endTop={471}></Square>
+                <Square
+                  startLeft={360}
+                  startTop={352}
+                  endLeft={986}
+                  endTop={427}></Square>
+                <Square
+                  startLeft={178}
+                  startTop={410}
+                  endLeft={810}
+                  endTop={610}></Square>
+                <Square
+                  startLeft={291}
+                  startTop={429}
+                  endLeft={919}
+                  endTop={566}></Square>
+                <Square
+                  startLeft={231}
+                  startTop={536}
+                  endLeft={331}
+                  endTop={536}></Square>
+                <Square
+                  startLeft={343}
+                  startTop={514}
+                  endLeft={365}
+                  endTop={402}></Square>
               </>
             }
             right_results={
               <>
-                <Circle left={'left-[776px]'} top={'top-[252px]'}></Circle>
-                <Circle left={'left-[686px]'} top={'top-[319px]'}></Circle>
-                <Circle left={'left-[808px]'} top={'top-[341px]'}></Circle>
-                <Circle left={'left-[634px]'} top={'top-[418px]'}></Circle>
-                <Circle left={'left-[757px]'} top={'top-[429px]'}></Circle>
-                <Circle left={'left-[881px]'} top={'top-[417px]'}></Circle>
-                <Circle left={'left-[684px]'} top={'top-[518px]'}></Circle>
-                <Circle left={'left-[765px]'} top={'top-[545px]'}></Circle>
-                <Circle left={'left-[850px]'} top={'top-[524px]'}></Circle>
+                <Circle
+                  startLeft={776}
+                  startTop={252}
+                  endLeft={766}
+                  endTop={378}></Circle>
+                <Circle
+                  startLeft={686}
+                  startTop={319}
+                  endLeft={83}
+                  endTop={214}></Circle>
+                <Circle
+                  startLeft={808}
+                  startTop={341}
+                  endLeft={142}
+                  endTop={297}></Circle>
+                <Circle
+                  startLeft={634}
+                  startTop={418}
+                  endLeft={82}
+                  endTop={362}></Circle>
+                <Circle
+                  startLeft={757}
+                  startTop={429}
+                  endLeft={270}
+                  endTop={356}></Circle>
+                <Circle
+                  startLeft={881}
+                  startTop={417}
+                  endLeft={61}
+                  endTop={440}></Circle>
+                <Circle
+                  startLeft={684}
+                  startTop={518}
+                  endLeft={172}
+                  endTop={418}></Circle>
+                <Circle
+                  startLeft={765}
+                  startTop={545}
+                  endLeft={195}
+                  endTop={534}></Circle>
+                <Circle
+                  startLeft={850}
+                  startTop={524}
+                  endLeft={275}
+                  endTop={462}></Circle>
               </>
             }></ResultsHeader>
           {/* Final Positions */}
           {/* <ResultsHeader
             left_results={
               <>
-                <Circle left={'left-[83px]'} top={'top-[214px]'}></Circle>
+                <Circle left={'left-[83px]'} top={214px]'}></Circle>
                 <Circle left={'left-[173px]'} top={'top-[207px]'}></Circle>
                 <Circle left={'left-[142px]'} top={'top-[297px]'}></Circle>
                 <Circle left={'left-[82px]'} top={'top-[362px]'}></Circle>
